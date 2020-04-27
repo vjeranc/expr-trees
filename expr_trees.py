680b2ccef6f49a4b888f7e1e4c181c89cf9ce7ef
Vjeran Crnjak <vjeran@crnjak.xyz>
2020-04-27 16:05:50 +0200

    changes: - cleanup of unused functions - parallel version that works much better and faster (maybe has bugs) - shared memory is unusable without numpy so have to add that

--- expr_trees.py
+++ expr_trees.py
@@ -1,8 +1,10 @@
 from collections import namedtuple
 import itertools as it
-from multiprocessing import Process
+from multiprocessing import Process, shared_memory
+from multiprocessing.managers import SharedMemoryManager
 import time
 import mpmath as mp
+import numpy as np
 
 Op = namedtuple('Op', ['symbol', 'priority', 'associative'])
 
@@ -11,6 +13,9 @@ Minus = Op('-', 0, False)
 Mul = Op('*', 1, True)
 Div = Op('/', 1, False)
 Pow = Op('^', 2, False)
+BaseConv = Op('_', 3, False)
+# Variable used later.
+_OPS = [Plus, Minus, Mul, Div, Pow, BaseConv]
 
 
 def gen_expr_trees(n, u, i):
@@ -33,12 +38,16 @@ def expr_trees(n, u, i):
     return s
 
 
-def all_same(ops):
-    return len(ops) <= 1
-
-
-def all_assoc(ops):
-    return all(op.associative for _, op in ops)
+def gen_psfx(ls, ops):
+    if len(ls) == 1:
+        yield ls
+        return
+    for op in ops:
+        for i in range(1, len(ls)):
+            r1 = psfx(ls[:i], ops)
+            r2 = psfx(ls[i:], ops)
+            for a, b in it.product(r1, r2):
+                yield a + b + op
 
 
 def psfx(ls, ops):
@@ -54,27 +63,17 @@ def psfx(ls, ops):
     return r
 
 
-_P = {  # priority
-    '+': 0,
-    '-': 0,
-    '*': 1,
-    '/': 1,
-    '^': 2
-}
-_ASSOC = set("+*")
-_OPERATORS = set("+-*/^")
-
-
-def _left_tree_ops(cur_op, all_ops):
-    pp = _P[cur_op]
-    return ''.join(
-        op for op in all_ops
-        if _P[op] < pp or _P[op] > pp or op not in _ASSOC
-    )
+_P = dict((op.symbol, op.priority) for op in _OPS)
+_ASSOC = set([op.symbol for op in _OPS if op.associative])
+_OPERATORS = set([op.symbol for op in _OPS])
 
 
 def td_psfx_cnt(n, all_ops):
     m = dict()
+    reduced_tree_ops = dict((cur_op, ''.join(
+        op for op in all_ops
+        if _P[op] < _P[cur_op] or _P[op] > _P[cur_op] or op not in _ASSOC
+    )) for cur_op in all_ops)
 
     def recur(n, ops):
         if n == 1:
@@ -83,7 +82,7 @@ def td_psfx_cnt(n, all_ops):
             return m[(n, ops)]
         r = 0
         for op in ops:
-            lops = _left_tree_ops(op, all_ops)
+            lops = reduced_tree_ops[op]
             rops = all_ops
             for i in range(1, n):
                 r1 = recur(i, lops)
@@ -95,12 +94,17 @@ def td_psfx_cnt(n, all_ops):
 
 
 def td_psfx(vs, all_ops):
+    reduced_tree_ops = dict((cur_op, ''.join(
+        op for op in all_ops
+        if _P[op] < _P[cur_op] or _P[op] > _P[cur_op] or op not in _ASSOC
+    )) for cur_op in all_ops)
+
     def recur(ls, ops):
         if len(ls) == 1:
             return ls
         r = []
         for op in ops:
-            lops = _left_tree_ops(op, all_ops)
+            lops = reduced_tree_ops[op]
             rops = all_ops
             for i in range(1, len(ls)):
                 r1 = recur(ls[:i], lops)
@@ -112,12 +116,17 @@ def td_psfx(vs, all_ops):
 
 
 def gen_td_psfx(vs, all_ops):
+    reduced_tree_ops = dict((cur_op, ''.join(
+        op for op in all_ops
+        if _P[op] < _P[cur_op] or _P[op] > _P[cur_op] or op not in _ASSOC
+    )) for cur_op in all_ops)
+
     def recur(ls, ops):
         if len(ls) == 1:
             yield ls
             return
         for op in ops:
-            lops = _left_tree_ops(op, all_ops)
+            lops = reduced_tree_ops[op]
             rops = all_ops
             for i in range(1, len(ls)):
                 r1 = recur(ls[:i], lops)
@@ -141,20 +150,6 @@ def seq_cnts():
             s = "123456789"[:i]
             seq.append(str(sum(1 for _ in td_psfx(s, op_set))))
         print(''.join(list(op_set)), ' '.join(seq))
-
-
-def gen_psfx(ls, lops=None, rops=None, tree='l'):
-    rops = rops or lops
-    ops = lops if tree == 'l' else rops
-    if len(ls) == 1:
-        yield ls
-        return
-    for op in ops:
-        for i in range(1, len(ls)):
-            r1 = gen_psfx(ls[:i], lops, rops, tree='l')
-            r2 = gen_psfx(ls[i:], lops, rops, tree='r')
-            for a, b in it.product(r1, r2):
-                yield (a + b + op)
 
 
 def choose_sum(n, i, choices):
@@ -193,8 +188,7 @@ def eval_psfx(pf):
             return None
         try:
             return _OPERATORS[op](a, b)
-        except Exception as e:
-            print(e)
+        except Exception:
             return None
 
     s = []
@@ -208,20 +202,18 @@ def eval_psfx(pf):
     return s.pop()[0]
 
 
-def _apply_op(at, bt, op):
-    a, op1 = at
-    b, op2 = bt
-    left_b = len(a) > 1 and _P[op1] < _P[op]
-    right_b = len(b) > 1 and (
-        _P[op2] < _P[op] or
-        _P[op2] == _P[op] and op not in _ASSOC)
-    s = " " + op + " "
-    s = ("({:})" if left_b else "{:}") + s
-    s = s + ("({:})" if right_b else "{:}")
-    return s.format(a, b)
-
-
 def postfix_to_infix(pf):
+    def _apply_op(at, bt, op):
+        a, op1 = at
+        b, op2 = bt
+        left_b = len(a) > 1 and _P[op1] < _P[op]
+        right_b = len(b) > 1 and (
+            _P[op2] < _P[op] or
+            _P[op2] == _P[op] and op not in _ASSOC)
+        s = " " + op + " "
+        s = ("({:})" if left_b else "{:}") + s
+        s = s + ("({:})" if right_b else "{:}")
+        return s.format(a, b)
     s = []
     for c in pf:
         if c not in _OPERATORS:
@@ -231,10 +223,6 @@ def postfix_to_infix(pf):
         d = _apply_op(a, b, c)
         s.append((d, c))
     return s.pop()[0]
-
-
-def eval_f(pf_expr):
-    print(postfix_to_infix(pf_expr), eval_psfx(pf_expr))
 
 
 def chunker(seq, size):
@@ -250,52 +238,134 @@ def infxR_to_psfx_map(pfs):
     return m
 
 
-def eval_expr(n, goal):
-    m = infxR_to_psfx_map(n, "+-*/^")
-    for k, vs in m.items():
-        print(k, end=' ', flush=True)
-        print(eval_psfx(vs[-1]))
+def eval_f(task_mem_name, res_mem_name, status_mem_name, start_time_mem_name,
+           cur_idx_mem_name,
+           status_idx, start_idx, end_idx, goal,
+           arr_dim, arr_dtype):
+    goal = mp.mpc(goal)
+
+    pfs_mem = shared_memory.SharedMemory(name=task_mem_name)
+    pfs = np.ndarray(arr_dim, dtype=arr_dtype, buffer=pfs_mem.buf)
+    res_mem = shared_memory.SharedMemory(name=res_mem_name)
+    res = np.ndarray(len(res_mem.buf), dtype=np.int8, buffer=res_mem.buf)
+    status = shared_memory.ShareableList(name=status_mem_name)
+    start_time = shared_memory.ShareableList(name=start_time_mem_name)
+    cur_idx_mem = shared_memory.SharedMemory(name=cur_idx_mem_name)
+    cur_idx = np.ndarray(
+        len(cur_idx_mem.buf)//8, dtype=np.int64, buffer=cur_idx_mem.buf)
+    status[status_idx] = "ACQMEM"
+    for i in range(start_idx, end_idx):
+        status[status_idx] = "CALC"
+        start_time[status_idx] = time.time()
+        cur_idx[status_idx] = i
+        res[i] = -1
+        r = eval_psfx(pfs[i].decode('ascii'))
+        res[i] = r == goal
+    status[status_idx] = "DONE"
 
 
-def parallel_eval_expr(n, goal):
+def parallel_eval_expr(n, ops, goal, timeout=5, proc_cnt=5):
     s = "123456789"[:n]
-    q = gen_td_psfx(s, "+-*/^")
-    ps = [Process(target=eval_f, args=("", "12+", 3))] * 100
-    pm = dict()
-    too_long = []
-    no_more = False
-    while not no_more:
-        nps, aps = [], []
-        for p in ps:
-            if p.is_alive():
-                if (time.time() - pm[p][1]) > 10:
-                    p.kill()
-                    nps.append(p)
-                    too_long.append(pm[p][2])
-                    del pm[p]
-                    continue
-                aps.append(p)
-            else:
-                nps.append(p)
-        for p in nps:
-            try:
-                p.close()
-            except Exception:
-                aps.append(p)
-                continue
-            try:
-                k = next(q)
-            except StopIteration:
-                no_more = True
-                break
-            p = Process(target=eval_f, args=(k, ))
-            p.start()
-            pm[p] = (p, time.time(), k)
-            aps.append(p)
-            if not q:
-                break
-        ps = aps
-        time.sleep(0.01)
+    q_type = np.dtype('a' + str(2*len(s)))
+    q = np.array(td_psfx(s, ops), dtype=q_type)
+    q.sort()
+    task_cnt = len(q)
+    task_block_size = task_cnt // proc_cnt
 
-    for tl in too_long:
-        print(tl)
+    task_ranges = []
+    for i in range(proc_cnt):
+        start_idx = i * task_block_size
+        end_idx = (i + 1) * task_block_size if i + 1 != proc_cnt else task_cnt
+        task_ranges.append((start_idx, end_idx))
+    smm = SharedMemoryManager()
+    smm.start()
+    task_mem = smm.SharedMemory(size=q.nbytes)
+    tq = np.ndarray(len(q), dtype=q_type, buffer=task_mem.buf)
+    tq[:] = q[:]
+    del q
+    res_mem = smm.SharedMemory(size=len(tq))
+    res = np.ndarray(len(tq), dtype=np.int8, buffer=res_mem.buf)
+    start_time_mem = smm.ShareableList([0.] * proc_cnt)
+    status_mem = smm.ShareableList(["IDLE"] * proc_cnt)
+    cur_idx_mem = smm.SharedMemory(size=proc_cnt * 8)
+    cur_idx = np.ndarray(proc_cnt, dtype=np.int64, buffer=cur_idx_mem.buf)
+    processes = [None] * proc_cnt
+    restarts = [0] * proc_cnt
+    cur_t = time.time()
+    while not all(status_mem[i] == "DONE" for i in range(proc_cnt)):
+        try:
+            for i, p in enumerate(processes):
+                if p is None:
+                    continue
+                status = status_mem[i]
+                if status == "DONE":
+                    continue
+                start_t = start_time_mem[i]
+                cur_idx = cur_idx[i]
+                if (cur_t - start_t) > timeout:
+                    p.kill()
+                    if cur_idx < 0:
+                        continue
+                    res[cur_idx] = -1
+
+            for i, p in enumerate(processes):
+                if p is not None and p.is_alive():
+                    continue
+                start_t = start_time_mem[i]
+                status = status_mem[i]
+                idx = cur_idx[i]
+                end_idx = task_ranges[i][1]
+                if status == "DONE":
+                    try:
+                        p.close()
+                    except Exception:
+                        pass
+                    processes[i] = None
+                    continue
+                try:
+                    p.close()
+                except Exception:
+                    pass
+                restarts[i] += 1
+                processes[i] = Process(
+                    target=eval_f,
+                    args=(task_mem.name,
+                          res_mem.name,
+                          status_mem.shm.name,
+                          start_time_mem.shm.name,
+                          cur_idx_mem.name,
+                          i,
+                          idx + 1,  # skipping the one that takes too long
+                          end_idx,
+                          goal,
+                          len(tq), q_type)
+                )
+                processes[i].start()
+        except Exception as e:
+            print(e)
+
+        cur_t = time.time()
+        time.sleep(max(1, timeout - 1))
+        print("Status: {:}|Found: {:d}".format(' '.join(
+                status_mem[i] + " " + str(r)
+                for (r, i) in zip(restarts, range(proc_cnt))
+            ), np.sum(res > 0))
+        )
+
+    for p in processes:
+        if p is None:
+            continue
+        p.join()
+        p.close()
+
+    for i, pf in enumerate(tq):
+        pf = pf.decode('ascii')
+        m = res[i]
+        if m == -1:
+            print("TLE", pf)
+            continue
+        if m == 0:
+            continue
+        print(eval_psfx(pf), '==', postfix_to_infix(pf), pf)
+
+    smm.shutdown()
